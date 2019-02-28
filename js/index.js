efd6148891115e1c669e35a272c0decf950cf3c9
jackgipson <jackjgipson@gmail.com>
2019-02-28 11:48:58 -0800

    finished home page v1 with hamburger functioning but need to fix

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // JS goes here
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+};
+
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', e => {
+    toggleMenu();
+  });
+
+
+
+
+
+
+
+
+
+// const toggleMenu = () => {
+//     // Toggle the "menu--open" class on your menu refence. 
+//     menu.classList.toggle('menu--open'); // this puts the class onto html but is not shown in html file itself
+//   };
+  
+//   // Start Here: Create a reference to the ".menu" class
+//   const menu = document.querySelector('.menu'); // This is creating a reference to the .menu class
+  
+//   // create a reference to the ".menu-button" class
+//   const menuButton = document.querySelector('.menu-button');
+  
+//   // Using your menuButton reference, add a click handler that calls toggleMenu
+//   menuButton.addEventListener('click', e => {
+//     toggleMenu();
+//   });
+  
+//   TweenMax.to('.menu-button', 10, {left:600, ease:Elastic.easeOut});  // animation sliding the menu hamburger with elastic snapback
+  
